e00640fcd156d657e887c31ecce694997e512e45
codybartfast <john@codybartfast.com>
2026-02-24 17:36:02 +0000

    🎄

--- day01.py
+++ day01.py
@@ -6,23 +6,22 @@
 #
 #  Timings
 #  ---------------------
-#    Parse:     0.000018
-#   Part 1:     0.000011
-#   Part 2:     0.000121
-#  Elapsed:     0.000192
+#    Parse:     0.000022
+#   Part 1:     0.000012
+#   Part 2:     0.000071
+#  Elapsed:     0.000148
+
 
 DIRS = ((0, -1), (1, 0), (0, 1), (-1, 0))
 NORTH = 0
 
 
 def parse(text):
-    def parse_line(line):
-        return [parts for parts in line.split()]
-
     return [(1 if line[0] == "R" else -1, int(line[1:])) for line in text.split(", ")]
 
 
 def part1(instructions, args, state_for_part2):
+    print(len(instructions))
     dir = NORTH
     (x, y) = (0, 0)
     for turn, count in instructions:
@@ -38,20 +37,15 @@ def part2(instructions, args, state_from_part1):
     dir = NORTH
     (x, y) = (0, 0)
     visited = set([(x, y)])
-    revisited = None
     for turn, count in instructions:
         dir = (dir + turn) % 4
         dx, dy = DIRS[dir]
         for _ in range(count):
             x += dx
             y += dy
-            if (x, y) in visited and not revisited:
-                revisited = (x, y)
-            else:
-                visited.add((x, y))
-
-    if revisited:
-        return abs(revisited[0]) + abs(revisited[1])
+            if (x, y) in visited:
+                return abs(x) + abs(y)
+            visited.add((x, y))
 
 
 def jingle(filename=None, filepath=None, text=None, extra_args=None):
