b6dbdc9e2f08e2e9a7f593359365a8001c385778
codybartfast <john@codybartfast.com>
2026-02-24 20:28:27 +0000

    🎄

--- day02.py
+++ day02.py
@@ -1,8 +1,23 @@
+#  Day 02
+#  ======
+#
+#  Part 1: 56855
+#  Part 2: B3C27
+#
+#  Timings
+#  ---------------------
+#    Parse:     0.000003
+#   Part 1:     0.000159
+#   Part 2:     0.000212
+#  Elapsed:     0.000416
+
+
+SQUARE_PAD = ["123", "456", "789"]
+DIAMOND_PAD = ["  1  ", " 234 ", "56789", " ABC ", "  D  "]
+
+
 def parse(text):
     return text.splitlines()
-
-
-square_pad = ["123", "456", "789"]
 
 
 def press_square(x, y, instrs):
@@ -18,17 +33,15 @@ def press_square(x, y, instrs):
                     y = min(2, y + 1)
                 case "L":
                     x = max(0, x - 1)
-        keys.append(square_pad[y][x])
+        keys.append(SQUARE_PAD[y][x])
     return keys
 
-
-diamond_pad = ["  1  ", " 234 ", "56789", " ABC ", "  D  "]
 
 def press_diamond(x, y, instrs):
     keys = []
     for seq in instrs:
         for dir in seq:
-            (x2, y2) = (x, y)
+            x2, y2 = x, y
             match dir:
                 case "U":
                     y2 -= 1
@@ -40,16 +53,16 @@ def press_diamond(x, y, instrs):
                     x2 -= 1
             if abs(x2 - 2) + abs(y2 - 2) < 3:
                 x, y = x2, y2
-        keys.append(diamond_pad[y][x]) 
+        keys.append(DIAMOND_PAD[y][x])
     return keys
 
 
-def part1(data, args, state_for_part2):
-    return "".join(press_square(1, 1, data))
+def part1(instrs, args, state_for_part2):
+    return "".join(press_square(1, 1, instrs))
 
 
-def part2(data, args, state_from_part1):
-    return "".join(press_diamond(0, 2, data))
+def part2(instrs, args, state_from_part1):
+    return "".join(press_diamond(0, 2, instrs))
 
 
 def jingle(filename=None, filepath=None, text=None, extra_args=None):
