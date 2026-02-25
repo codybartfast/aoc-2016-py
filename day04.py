21e68fd50654c8068474166ba9192233140afc37
codybartfast <john@codybartfast.com>
2026-02-25 07:46:19 +0000

    day4 part2

--- day04.py
+++ day04.py
@@ -1,25 +1,37 @@
-
-# intentionally overusing itertools :-)
 from itertools import groupby
+
+CHARS = "abcdefghijklmnopqrstuvwxyz"
+
 
 def parse(text):
     def parse_line(line):
-        parts = line[:-7].split("-")
-        return parts[:-1], int(parts[-1]), line[-6:-1]
+        return line[:-11], int(line[-10:-7]), line[-6:-1]
+
     return [parse_line(line) for line in text.splitlines()]
 
 
-def check_sum(name_parts):
-    groups = [(key, list(grpr)) for key, grpr in groupby(sorted("".join(name_parts)))]
-    return "". join(key for key, _ in sorted(groups, key=lambda grp: (-len(grp[1]), grp[0])))[:5]
+def checksum(name):
+    groups = ((key, list(grpr)) for key, grpr in groupby(sorted(name)))
+    return "".join(
+        key
+        for key, _ in sorted(groups, key=lambda grp: (-len(grp[1]), grp[0]))
+        if key != "-"
+    )[:5]
 
 
-def part1(data, args, state_for_part2):
-    return sum(room[1] for room in data if check_sum(room[0])==room[2])
+def part1(rooms, args, state_for_part2):
+    return sum(room[1] for room in rooms if checksum(room[0]) == room[2])
 
 
-def part2(data, args, state_from_part1):
-    return "ans2"
+def part2(rooms, args, state_from_part1):
+    for room in rooms:
+        encrpted, sector, csum = room
+        if checksum(encrpted) == csum:
+            shift = sector % 26
+            trans = str.maketrans(CHARS, CHARS[shift:] + CHARS[:shift])
+            name = encrpted.translate(trans)
+            if "north" in name:
+                return sector
 
 
 def jingle(filename=None, filepath=None, text=None, extra_args=None):
