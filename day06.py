1f10f5d138d3418e2b05cb591173f9b1908dfe5f
codybartfast <john@codybartfast.com>
2026-02-26 12:11:02 +0000

    day6 part1

---
+++ day06.py
@@ -0,0 +1,30 @@
+from collections import Counter
+
+def parse(text):
+    return text.splitlines()
+
+
+def part1(repitions, args, state_for_part2):
+    return "".join(Counter(col).most_common(1)[0][0] for col in zip(*repitions))
+
+
+def part2(data, args, state_from_part1):
+    return "ans2"
+
+
+def jingle(filepath=None, text=None, extra_args=None):
+    if not text and filepath:
+        with open(filepath, "r") as f:
+            text = f.read().strip()
+    sack.present(text, extra_args, parse, part1, part2)
+
+
+if __name__ == "__main__":
+    import sys
+    import sack
+
+    file = sys.argv[1] if len(sys.argv) > 1 else None
+    filepath = sack.get_filepath(file)
+    if filepath:
+        extra_args = sys.argv[1:] if len(sys.argv) > 1 else []
+        jingle(filepath=filepath, extra_args=extra_args)
