2e75e1374ad718141f1d640cd007fb21b20bfc48
codybartfast <john@codybartfast.com>
2026-02-26 11:44:41 +0000

    🐰

--- day01.py
+++ day01.py
@@ -48,16 +48,19 @@ def part2(instructions, args, state_from_part1):
             visited.add((x, y))
 
 
-def jingle(filename=None, filepath=None, text=None, extra_args=None):
-    import sack
-
-    text = text if text else sack.read_input(filename, filepath)
+def jingle(filepath=None, text=None, extra_args=None):
+    if not text and filepath:
+        with open(filepath, "r") as f:
+            text = f.read().strip()
     sack.present(text, extra_args, parse, part1, part2)
 
 
 if __name__ == "__main__":
     import sys
+    import sack
 
-    filename = sys.argv[1] if len(sys.argv) > 1 else None
-    extra_args = sys.argv[1:] if len(sys.argv) > 2 else None
-    jingle(filename=filename, extra_args=extra_args)
+    file = sys.argv[1] if len(sys.argv) > 1 else None
+    filepath = sack.get_filepath(file)
+    if filepath:
+        extra_args = sys.argv[1:] if len(sys.argv) > 1 else []
+        jingle(filepath=filepath, extra_args=extra_args)
