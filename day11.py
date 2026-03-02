6c95dafb3e3b480fc96de5bdd7238f603964c6ea
codybartfast <john@codybartfast.com>
2026-03-02 17:46:16 +0000

    well it works

--- day11.py
+++ day11.py
@@ -1,33 +1,7 @@
 import re
 
-
-class Facility:
-    def __init__(self, floors):
-        self.floors = floors
-        self.size = len(floors)
-        self.flr = self.size - 1
-        self.count = 0
-        self.have_collected_gens = False
-
-    def gens(self, flr=None):
-        if flr is None:
-            flr = self.flr
-        return self.floors[flr][0]
-
-    def chips(self, flr=None):
-        if flr is None:
-            flr = self.flr
-        return self.floors[flr][1]
-
-    def find_gen(self, gen):
-        for flr in range(self.size):
-            if gen in self.gens(flr):
-                return flr
-
-    def find_chip(self, chip):
-        for flr in range(self.size):
-            if chip in self.chips(flr):
-                return flr
+UP = +1
+DOWN = -1
 
 
 def parse(text):
@@ -39,162 +13,159 @@ def parse(text):
             floor[0 if type == "generator" else 1].append(material[:2].title())
         return floor
 
-    floors = [parse_line(line) for line in reversed(text.splitlines())]
+    floors = [parse_line(line) for line in (text.splitlines())]
     return Facility(floors)
 
 
 # reversed floors
 
-# Strategy?
-#   - if at top:
-#     - done?
-#     - move a chip down
-#   - try:
-#     - move a pair up
-#     - ? move a gen up
-#     - pair a chip
-#       - up (but not top)
-#       - down
-#     - move so can pair
+# Strategy #3?
+#   - clear all from 1st except one chip
+#   - move chips down from 3rd except one chip
+#   - move up first pair
+#   - jiggle up remaining pairs on 2nd, but one
+#   - move up last 2nd pair
+#   - move up 1st chip
 
 
-def display(facility: Facility):
-    print()
-    print(facility.flr)
-    for i, floor in enumerate(facility.floors):
-        print("E" if i == facility.flr else " ", floor)
-        # print(floor)
-    print("count:", facility.count)
+class Facility:
+    def __init__(self, floors):
+        self.floors = floors
+        self.size = len(floors)
+        self.flr = 0
+        self.count = 0
+        self.have_collected_gens = False
+
+    def display(self):
+        lines = [f"{"E" if i == self.flr else " "} {floor}" for i, floor in enumerate(self.floors)]
+        print("\n".join(reversed(lines)))
+        print("count:", self.count)
+        print()
+
+    def done(self):
+        return not any(lst for floor in self.floors for lst in floor)
+
+    def gens(self, flr=None):
+        if flr is None:
+            flr = self.flr
+        return self.floors[flr][0]
+
+    def chips(self, flr=None):
+        if flr is None:
+            flr = self.flr
+        return self.floors[flr][1]
+
+    def is_valid(self):
+        for floor in self.floors:
+            if floor[1] and floor[0] and any(kind not in floor[0] for kind in floor[1]):
+                assert False  # Invalid State
+        return True
+
+    def find_gen(self, gen):
+        for flr in range(self.size):
+            if gen in self.gens(flr):
+                return flr
+
+    def find_chip(self, chip):
+        for flr in range(self.size):
+            if chip in self.chips(flr):
+                return flr
+
+    def by_matched(self, flr=None):
+        if flr is None:
+            flr = self.flr
+        gens, chips = self.gens(flr), self.chips(flr)
+        # print(self.flr, self.floors[self.flr], gens, chips)
+        pairs = [el for el in gens if el in chips]
+        lone_gens = [el for el in gens if el not in pairs]
+        lone_chips = [el for el in chips if el not in pairs]
+        return lone_gens, pairs, lone_chips
+
+    def move(self, dir, gens, chips):
+        print(
+            f"Moving {'Up' if dir == UP else 'Down'}, generators:{gens}, chips:{chips}"
+        )
+        size = len(gens) + len(chips)
+        if dir == UP and size == 1:
+            print("***************")
+        assert 0 < size < 3 , "move takes 1 or 2 items"
+        dest = self.flr + dir
+        for gen in gens:
+            self.gens().remove(gen)
+            self.gens(dest).append(gen)
+        for chip in chips:
+            self.chips().remove(chip)
+            self.chips(dest).append(chip)
+        self.count += 1
+        self.flr = dest
+        self.display()
+        self.is_valid()
 
 
-def is_valid(facility: Facility):
-    for floor in facility.floors:
-        if floor[1] and floor[0] and any(kind not in floor[0] for kind in floor[1]):
-            assert False  # Invalid State
-    return True
+def isolate_chips_on_1st(facility):
+    # assumes lift on 1st, leaves it on 2nd
+    match facility.by_matched():
+        case ([], [pair], []):
+            facility.move(UP, [pair], [pair])
+            return pair
 
 
-def done(facility: Facility):
-    return not any(lst for floor in facility.floors for lst in floor)
+def clear_most_from_3rd(facility, chap):
+    # assumes lift on 2nd, leaves on 2nd
+    while True:
+        match facility.by_matched(2):
+            case ([], [], [chip1, chip2]):
+                facility.move(UP, [chip1, chip2], [])
+                facility.move(UP, [chip1], [chip1])
+                facility.move(DOWN, [chip1], [])
+                facility.move(UP, [chip1, chip2], [])
+                facility.move(DOWN, [chip2], [])
+                facility.move(DOWN, [chip2], [])
+                facility.move(DOWN, [chap], [chap])
+                facility.move(UP, [chap], [])
+                return None, chip2
+            case ([], [], [chip, _, *_]):
+                facility.move(UP, [], [chap])
+                facility.move(DOWN, [], [chip, chap])
+            case _:
+                raise RuntimeError("oops")
 
 
-def is_vulnerable(floor):
-    gens, chips = floor
-    return any(chip for chip in chips if chip not in gens)
+def move_pairs_from_2nd_to_4th(facility, chap, leading_chip=None):
+    def move_pair(pair):
+        if not leading_chip:
+            facility.move(UP, [pair], [pair])
+            facility.move(DOWN, [pair], [])
+        facility.move(UP, [chap, pair], [])
+        facility.move(UP, [chap, pair], [])
+        facility.move(DOWN, [pair], [])
+        facility.move(UP, [pair], [pair])
+        facility.move(DOWN, [chap], [])
+        facility.move(DOWN, [chap], [])
+
+        
+    if leading_chip:
+        move_pair(leading_chip)   
+        leading_chip = None
+    _, pairs, _ = facility.by_matched()
+    for pair in pairs:
+        move_pair(pair)
 
 
-def is_safe(facility, flr, chip):
-    if 0 <= flr < facility.size:
-        gens = facility.gens(flr)
-        return not gens or chip in gens
-
-
-def unit(n):
-    return n // abs(n)
-
-
-def next_floor(facility, dest):
-    return facility.flr + unit(dest - facility.flr)
-
-
-def move_pair_up(facility: Facility):
-    floors, flr = facility.floors, facility.flr
-    upper = floors[flr - 1]
-    if is_vulnerable(upper):
-        # print("m is vulnerable")
-        return None
-    gens, chips = floors[flr]
-    pairs = [el for el in gens if el in chips]
-    if not pairs:
-        # print("m no pairs")
-        return None
-
-    print("m start")
-    pair = pairs[0]
-    dest = facility.flr - 1
-    # print("Flr:", facility.flr, "Dest:", dest, "Pair:", pair)
-    facility.chips().remove(pair)
-    facility.gens().remove(pair)
-    # display(facility)
-    facility.chips(dest).append(pair)
-    facility.gens(dest).append(pair)
-    # display(facility)
-    facility.flr = dest
-    facility.count += 1
-    # print("m done")
-    return facility
-
-
-def drop_something(facility):
-    flr = 1
-    while not facility.gens(flr) and not facility.chips(flr):
-        flr += 1
-    if facility.gens(flr):
-        facility.gens(flr).append(facility.gens().pop())
-    else:
-        facility.chips(flr).append(facility.chips().pop())
-    facility.count += flr
-    facility.flr = flr
-    return facility
-
-
-def reunite_chip(facility):
-    unmatched = [chip for chip in facility.chips() if chip not in facility.gens()]
-    if not unmatched:
-        return None
-
-    print("ru start")
-    opts = [(chip, next_floor(facility, facility.find_gen(chip))) for chip in unmatched]
-    if len(set(opt[1] for opt in opts)) != 1:
-        assert False  # need to make choices
-    for opt in opts[:2]:
-        chip, dest = opt
-        facility.chips().remove(chip)
-        facility.chips(dest).append(chip)
-    facility.flr = opts[0][1]
-    facility.count += 1
-    return facility
-
-
-def reposition(facility):
-    print("rp start")
-    unmatched = []
-    for flr in range(facility.size):
-        for unm in (
-            chip for chip in facility.chips(flr) if chip not in facility.gens(flr)
-        ):
-            unmatched.append(flr)
-    assert unmatched
-    unmatched.sort(key=lambda unm_flr: abs(unm_flr - facility.flr))
-    new_flr = unmatched[0]
-    if abs(new_flr - facility.flr) > 1:
-        assert False  # think 1) safe 2) loops 3) correct count
-    facility.chips(new_flr).append(facility.chips().pop())
-    facility.flr = new_flr
-    facility.count += 1
-    return facility
-
-
-def move_things(facility):
-    while not done(facility):
-        display(facility)
-        is_valid(facility)
-        input("...")
-        if facility.flr == 0:
-            drop_something(facility)
-        else:
-            rslt = move_pair_up(facility)
-            if not rslt:
-                rslt = reunite_chip(facility)
-            if not rslt:
-                rslt = reposition(facility)
-
-    print("\nDone")
-    display(facility)
+def end(facility, chap):
+    facility.move(DOWN, [chap], [])
+    facility.move(UP, [chap], [chap])
+    facility.move(UP, [chap], [chap])
+    facility.move(UP, [chap], [chap])
 
 
 def part1(facility, args, p1_state):
-    move_things(facility)
+    facility.display()
+    chaperone = isolate_chips_on_1st(facility)
+    rmn_gen, rmn_chip = clear_most_from_3rd(facility, chaperone)
+    assert not rmn_gen
+    move_pairs_from_2nd_to_4th(facility, chaperone, rmn_chip)
+    end(facility, chaperone)
     return "ans1"
 
 
