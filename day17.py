ba330bcfae9cf3929cb3608618a4677350a4a818
codybartfast <john@codybartfast.com>
2026-03-08 19:24:46 +0000

    day17 part2

--- day17.py
+++ day17.py
@@ -2,42 +2,47 @@ from hashlib import md5
 
 DIRS = [("U", (0, -1)), ("D", (0, 1)), ("L", (-1, 0)), ("R", (1, 0))]
 
+
 def parse(text):
     return text.encode()
 
 
-def next_paths(paths, size):
+def next_paths(paths):
     npaths = []
+    vpaths = []
     for path in paths:
         (x, y), steps, hash = path
         for (dir, (dx, dy)), hash_char in zip(DIRS, hash.hexdigest()):
             nx, ny = x + dx, y + dy
-            if 0 <= nx < size and 0 <= ny < size and hash_char in "bcdef":
+            if 0 <= nx < 4 and 0 <= ny < 4 and hash_char in "bcdef":
                 nhash = hash.copy()
                 nhash.update(dir.encode())
-                npaths.append(((nx, ny), steps + [dir], nhash))
-    return npaths
-        
-    
+                (npaths if (nx, ny) != (3, 3) else vpaths).append(
+                    ((nx, ny), steps + [dir], nhash)
+                )
+    return npaths, vpaths
+
+
+def explore(code):
+    start = ((0, 0), [], md5(code))
+    paths = [start]
+    while paths:
+        # print(paths)
+        paths, vpaths = next_paths(paths)
+        if vpaths:
+            yield vpaths
 
 
 def part1(code, args, p1_state):
-    size = 4
-    end = (size - 1, size - 1)
-    print(f"{code}\n\n")
-    paths = [((0,0), [], md5(code))]
-    v_paths = None
-    while paths and not v_paths:
-        # print(paths)
-        paths = next_paths(paths, 4)
-        v_paths = [path for path in paths if path[0] == end]
-            
-
-    return "".join(v_paths[0][1])
+    explore_it = iter(explore(code))
+    p1_state.value = explore_it
+    return "".join(next(explore_it)[0][1])
 
 
 def part2(data, args, p1_state):
-    return "ans2"
+    vault_paths_list = list(p1_state.value)
+    last = vault_paths_list[-1][-1]
+    return len(last[1])
 
 
 def jingle(filepath=None, text=None, extra_args=None):
