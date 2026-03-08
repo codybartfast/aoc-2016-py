08e96406cf3e1592a6a3ab4b40a7d7d1259e414a
codybartfast <john@codybartfast.com>
2026-03-08 19:42:59 +0000

    🐰

--- day17.py
+++ day17.py
@@ -1,6 +1,20 @@
+#  2016 Day 17
+#  ===========
+#
+#  Part 1: RDRDUDLRDR
+#  Part 2: 386
+#
+#  Timings
+#  ---------------------
+#    Parse:     0.000001
+#   Part 1:     0.000060
+#   Part 2:     0.039628
+#  Elapsed:     0.039733
+
+
 from hashlib import md5
 
-DIRS = [("U", (0, -1)), ("D", (0, 1)), ("L", (-1, 0)), ("R", (1, 0))]
+DIRS = [(b"U", (0, -1)), (b"D", (0, 1)), (b"L", (-1, 0)), (b"R", (1, 0))]
 
 
 def parse(text):
@@ -8,40 +22,43 @@ def parse(text):
 
 
 def next_paths(paths):
-    npaths = []
-    vpaths = []
+    new_paths = []
+    vault_paths = []
     for path in paths:
         (x, y), steps, hash = path
-        for (dir, (dx, dy)), hash_char in zip(DIRS, hash.hexdigest()):
+        hexdigest = hash.hexdigest()
+        for i in range(4):
+            dir, (dx, dy) = DIRS[i]
+            hash_char = hexdigest[i]
             nx, ny = x + dx, y + dy
             if 0 <= nx < 4 and 0 <= ny < 4 and hash_char in "bcdef":
                 nhash = hash.copy()
-                nhash.update(dir.encode())
-                (npaths if (nx, ny) != (3, 3) else vpaths).append(
+                nhash.update(dir)
+                (vault_paths if (nx, ny) == (3, 3) else new_paths).append(
                     ((nx, ny), steps + [dir], nhash)
                 )
-    return npaths, vpaths
+    return new_paths, vault_paths
 
 
 def explore(code):
     start = ((0, 0), [], md5(code))
     paths = [start]
     while paths:
-        # print(paths)
-        paths, vpaths = next_paths(paths)
-        if vpaths:
-            yield vpaths
+        paths, vault_paths = next_paths(paths)
+        if vault_paths:
+            yield vault_paths
 
 
 def part1(code, args, p1_state):
     explore_it = iter(explore(code))
     p1_state.value = explore_it
-    return "".join(next(explore_it)[0][1])
+    vault_path = next(explore_it)[0]
+    return b"".join(vault_path[1]).decode()
 
 
-def part2(data, args, p1_state):
+def part2(code, args, p1_state):
     vault_paths_list = list(p1_state.value)
-    last = vault_paths_list[-1][-1]
+    last = vault_paths_list[-1][0]
     return len(last[1])
 
 
